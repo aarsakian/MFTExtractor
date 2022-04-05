4ff82f879ed5eba52727e10dad660a104a88bd26
Armen Arsakian <aarsakian@gmail.com>
2022-04-05 13:44:10 -0700

    added function to get physical drive size

--- img/img.go
+++ img/img.go
@@ -2,10 +2,19 @@ package img
 
 import (
 	"log"
+	"unsafe"
 
 	"github.com/aarsakian/MFTExtractor/utils"
 	"golang.org/x/sys/windows"
 )
+
+type DISK_GEOMETRY struct {
+	Cylinders         int64
+	MediaType         int32
+	TracksPerCylinder int32
+	SectorsPerTrack   int32
+	BytesPerSector    int32
+}
 
 func ReadDisk(a_file string, offset int64, bytesToRead uint32) []byte {
 
@@ -29,10 +38,35 @@ func ReadDisk(a_file string, offset int64, bytesToRead uint32) []byte {
 		log.Fatalln(err)
 	}
 
+	diskSize := getDiskSize(fd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if offset > int64(diskSize) {
+		log.Fatal("error offset exceeds file size ", offset-diskSize)
+	}
+
 	err = windows.ReadFile(fd, buf_pointer, &bytesRead, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	return buf_pointer
 
+}
+
+func getDiskSize(hD windows.Handle) int64 {
+	const IOCTL_DISK_GET_DRIVE_GEOMETRY = 0x70000
+	const nByte_DISK_GEOMETRY = 24
+	disk_geometry := DISK_GEOMETRY{}
+
+	var junk *uint32
+	var inBuffer *byte
+	err := windows.DeviceIoControl(hD, IOCTL_DISK_GET_DRIVE_GEOMETRY,
+		inBuffer, 0, (*byte)(unsafe.Pointer(&disk_geometry)), nByte_DISK_GEOMETRY, junk, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return disk_geometry.Cylinders * int64(disk_geometry.TracksPerCylinder) * int64(disk_geometry.SectorsPerTrack) * int64(disk_geometry.BytesPerSector)
 }
