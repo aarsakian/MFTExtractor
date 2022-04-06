32b2de6d0eb3a37f4f6d9f65db3f25fa115af2f9
Armen Arsakian <aarsakian@gmail.com>
2022-04-06 13:39:09 -0700

    support for ntfs based file extraction from  added

--- NTFS/ntfs.go
+++ NTFS/ntfs.go
@@ -1,17 +1,32 @@
 package ntfs
 
-type NFTS struct {
+import (
+	"fmt"
+
+	"github.com/aarsakian/MFTExtractor/img"
+	"github.com/aarsakian/MFTExtractor/utils"
+)
+
+type NTFS struct {
 	JumpInstruction   [3]byte //0-3
 	Signature         string  //4 bytes NTFS 3-7
 	NotUsed1          [4]byte
-	BytesPerSector    uint32   // 11-13
-	SectorsPerCluster uint8    //14
-	NotUsed2          [26]byte //14-40
-	TotalSectors      uint64   //40-48
+	BytesPerSector    uint16   // 11-13
+	SectorsPerCluster uint8    //13
+	NotUsed2          [26]byte //13-39
+	TotalSectors      uint64   //39-47
 	MFTOffset         uint64   //48-56
 	MFTMirrOffset     uint64   //56-64
 }
 
-func Parse(driveNumber int) {
+func Parse(driveNumber int) NTFS {
+	offset := int64(124769275 * 512)
+	length := uint32(512)
+	hD := img.GetHandler("\\\\.\\PHYSICALDRIVE" + fmt.Sprintf("%d", driveNumber))
 
+	defer img.CloseHandler(hD)
+	data := img.ReadDisk(hD, offset, length)
+	var ntfs NTFS
+	utils.Unmarshal(data, &ntfs)
+	return ntfs
 }
