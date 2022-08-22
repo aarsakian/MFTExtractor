b2ae708ee798c6af108c9e6da17d7b9b318c8a58
Armen Arsakian <arsakian@digitalforensics.gr>
2022-08-22 22:42:22 +0300

    interfaces to accomodate linux and window apis

---
+++ img/reader.go
@@ -0,0 +1,28 @@
+package img
+
+import (
+	"runtime"
+)
+
+type DiskReader interface {
+	CreateHandler()
+	CloseHandler()
+	ReadFile(int64, uint32) []byte
+	GetDiskSize() int64
+}
+
+func GetHandler(pathToDisk string) DiskReader {
+	os := runtime.GOOS
+	var dr DiskReader
+	switch os {
+	case "windows":
+		//dr = WindowsReader{pathToDisk: pathToDisk}
+
+	case "linux":
+		dr = UnixReader{pathToDisk: pathToDisk}
+
+	}
+	dr.CreateHandler()
+	defer dr.CloseHandler()
+	return dr
+}
---
+++ img/reader_windows.go
@@ -0,0 +1,75 @@
+package img
+
+import (
+	"log"
+	"unsafe"
+
+	"github.com/aarsakian/MFTExtractor/utils"
+	"golang.org/x/sys/windows"
+)
+
+type DISK_GEOMETRY struct {
+	Cylinders         int64
+	MediaType         int32
+	TracksPerCylinder int32
+	SectorsPerTrack   int32
+	BytesPerSector    int32
+}
+
+type WindowsReader struct {
+	a_file string
+	fd     windows.Handle
+}
+
+func (winreader WindowsReader) CreateHandler() {
+	file_ptr, _ := windows.UTF16PtrFromString(winreader.a_file)
+	var templateHandle windows.Handle
+	fd, err := windows.CreateFile(file_ptr, windows.FILE_READ_DATA,
+		windows.FILE_SHARE_READ, nil,
+		windows.OPEN_EXISTING, 0, templateHandle)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	winreader.fd = fd
+}
+
+func (winreader WindowsReader) CloseHandlerW() {
+	windows.Close(winreader.fd)
+}
+
+func (winreader WindowsReader) GetDiskSize() int64 {
+	const IOCTL_DISK_GET_DRIVE_GEOMETRY = 0x70000
+	const nByte_DISK_GEOMETRY = 24
+	disk_geometry := DISK_GEOMETRY{}
+
+	var junk *uint32
+	var inBuffer *byte
+	err := windows.DeviceIoControl(winreader.fD, IOCTL_DISK_GET_DRIVE_GEOMETRY,
+		inBuffer, 0, (*byte)(unsafe.Pointer(&disk_geometry)), nByte_DISK_GEOMETRY, junk, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return disk_geometry.Cylinders * int64(disk_geometry.TracksPerCylinder)
+	*int64(disk_geometry.SectorsPerTrack) * int64(disk_geometry.BytesPerSector)
+}
+
+func (winreader WindowsReader) ReadFile(buf_pointer int64, bytesRead uint32) []byte {
+
+	buf_pointer := make([]byte, bytesToRead)
+	largeInteger := utils.NewLargeInteger(offset)
+	var bytesRead uint32
+
+	newLowOffset, err := windows.SetFilePointer(winreader.fd, largeInteger.LowPart,
+		&largeInteger.HighPart, windows.FILE_BEGIN)
+	largeInteger.LowPart = int32(newLowOffset)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = windows.ReadFile(winreader.fd, buf_pointer, &bytesRead, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return buf_pointer
+}
