3db52ae28e10da70dae268edf0c2573738ec8d25
Armen Arsakian <aarsakian@gmail.com>
2022-08-22 22:42:22 +0300

    interfaces to accomodate linux and window apis

---
+++ img/reader_unix.go
@@ -0,0 +1,39 @@
+package img
+
+import (
+	"log"
+
+	"golang.org/x/sys/unix"
+)
+
+type UnixReader struct {
+	pathToDisk string
+	fd         int
+}
+
+func (unixreader UnixReader) CreateHandler() {
+	fd, err := unix.Open(unixreader.pathToDisk, unix.O_RDONLY, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	unixreader.fd = fd
+}
+
+func (unixreader UnixReader) ReadFile(buf_pointer int64, bytesToRead uint32) []byte {
+	buffer := make([]byte, bytesToRead)
+	unix.Seek(unixreader.fd, buf_pointer, unix.SEEK_SET)
+	_, err := unix.Read(unixreader.fd, buffer)
+	if err == nil {
+		log.Fatal("error reading")
+	}
+	return buffer
+
+}
+
+func (unixreader UnixReader) CloseHandler() {
+	unix.Close(unixreader.fd)
+}
+
+func (unixreader UnixReader) GetDiskSize() int64 {
+	return 0
+}
