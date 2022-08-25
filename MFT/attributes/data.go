584861437e78949d6d0bce8a707ec594a6c743f1
Armen Arsakian <aarsakian@gmail.com>
2022-08-25 10:14:17 +0300

    moved attributes to their files

---
+++ MFT/attributes/data.go
@@ -0,0 +1,25 @@
+package attributes
+
+type DATA struct {
+	Content []byte
+	Header  *AttributeHeader
+}
+
+func (data *DATA) SetHeader(header *AttributeHeader) {
+	data.Header = header
+}
+
+func (data DATA) GetHeader() AttributeHeader {
+	return *data.Header
+}
+
+func (data DATA) FindType() string {
+	return data.Header.GetType()
+}
+func (data DATA) IsNoNResident() bool {
+	return data.Header.IsNoNResident()
+}
+
+func (data DATA) ShowInfo() {
+
+}
