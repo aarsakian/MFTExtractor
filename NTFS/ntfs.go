1d1dc37f58f3e59cbcb9f637afc60acebcd8855c
aarsakian <aarsakian@gmail.com>
2023-04-22 01:44:35 -0700

    rewrote get mft entry considering mft fragmented file

--- NTFS/ntfs.go
+++ NTFS/ntfs.go
@@ -8,30 +8,48 @@ import (
 )
 
 type NTFS struct {
-	JumpInstruction   [3]byte //0-3
-	Signature         string  //4 bytes NTFS 3-7
-	NotUsed1          [4]byte
-	BytesPerSector    uint16   // 11-13
-	SectorsPerCluster uint8    //13
-	NotUsed2          [26]byte //13-39
-	TotalSectors      uint64   //39-47
-	MFTOffset         uint64   //48-56
-	MFTMirrOffset     uint64   //56-64
+	JumpInstruction           [3]byte //0-3
+	Signature                 string  //4 bytes NTFS 3-7
+	NotUsed1                  [4]byte
+	BytesPerSector            uint16       // 11-13
+	SectorsPerCluster         uint8        //13
+	NotUsed2                  [26]byte     //13-39
+	TotalSectors              uint64       //39-47
+	MFTOffset                 uint64       //48-56
+	MFTMirrOffset             uint64       //56-64
+	MFTrunlistOffsetsAndSizes *map[int]int //points to $MFT
 }
 
 func (ntfs NTFS) GetSectorsPerCluster() uint8 {
 	return ntfs.SectorsPerCluster
 }
 
-func (ntfs NTFS) GetMFTEntry(hD img.DiskReader, partitionOffset uint32, recordOffset int) []byte {
-	length := uint32(1024) // len of MFT record
-	physicalOffset := int64(ntfs.MFTOffset)*int64(ntfs.SectorsPerCluster)*512 +
-		int64(partitionOffset)*512 + int64(recordOffset)
-	buffer := make([]byte, length)
+func (ntfs NTFS) GetMFTEntry(hD img.DiskReader, partitionOffset uint32,
+	recordOffset int) []byte {
 
+	length := uint32(1024) // len of MFT record
+
+	buffer := make([]byte, length)
+	mftOffset := int64(ntfs.MFTOffset * uint64(ntfs.SectorsPerCluster) * 512)
+	offsetedRecord := recordOffset
+	lengthB := 0
+	if offsetedRecord > 0 {
+		for offset, len := range *ntfs.MFTrunlistOffsetsAndSizes {
+			lengthB += len * int(ntfs.SectorsPerCluster) * 512
+			if offsetedRecord >= lengthB { // more than the available clusters in the contiguous area
+				mftOffset = int64(offset * int(ntfs.SectorsPerCluster) * 512)
+				offsetedRecord -= lengthB
+				break
+			}
+
+		}
+
+	}
+
+	logicalOffsetB := int64(mftOffset) + int64(offsetedRecord)
+	physicalOffset := int64(partitionOffset)*512 + logicalOffsetB
 	hD.ReadFile(physicalOffset, buffer)
 	return buffer
-
 
 }
 
