64382998cd83f97374dea981ffb4f05572d46924
aarsakian <aarsakian@gmail.com>
2023-07-22 08:18:52 -0700

    new methods for working with  as a file

--- NTFS/ntfs.go
+++ NTFS/ntfs.go
@@ -1,8 +1,10 @@
 package ntfs
 
 import (
+	"bytes"
 	"fmt"
 
+	"github.com/aarsakian/MFTExtractor/MFT"
 	"github.com/aarsakian/MFTExtractor/img"
 	"github.com/aarsakian/MFTExtractor/utils"
 )
@@ -18,10 +20,47 @@ type NTFS struct {
 	MFTOffset                 uint64       //48-56
 	MFTMirrOffset             uint64       //56-64
 	MFTrunlistOffsetsAndSizes *map[int]int //points to $MFT
+	MFTRecords                []MFT.Record
+	MFTSize                   int
 }
 
 func (ntfs NTFS) GetSectorsPerCluster() uint8 {
 	return ntfs.SectorsPerCluster
+}
+
+func (ntfs NTFS) CollectMFTArea(hD img.DiskReader, partitionOffset uint64) []byte {
+	var buf bytes.Buffer
+
+	buf.Grow(ntfs.MFTSize) // allow for MFT size
+
+	partitionOffsetB := int64(partitionOffset) * int64(ntfs.BytesPerSector)
+
+	for offset, clustr := range *ntfs.MFTrunlistOffsetsAndSizes {
+		//inefficient since allocates memory for each round
+		tempBuffer := make([]byte, clustr*int(ntfs.SectorsPerCluster)*int(ntfs.BytesPerSector))
+		hD.ReadFile(partitionOffsetB+int64(offset)*int64(ntfs.SectorsPerCluster)*int64(ntfs.BytesPerSector), tempBuffer)
+		buf.Write(tempBuffer)
+	}
+	return buf.Bytes()
+}
+
+func (ntfs *NTFS) ProcessFirstRecord(hD img.DiskReader, partitionOffset uint64) {
+	bs := ntfs.GetMFTEntry(hD, partitionOffset, 0)
+	ntfs.ProcessRecords(bs)
+	firstRecord := ntfs.MFTRecords[0]
+	runlistOffsetsAndSizes := firstRecord.GetRunListSizesAndOffsets()
+	ntfs.MFTrunlistOffsetsAndSizes = &runlistOffsetsAndSizes
+
+	ntfs.MFTSize = int(firstRecord.GetTotalRunlistSize() * int(ntfs.BytesPerSector) * int(ntfs.SectorsPerCluster))
+}
+
+func (ntfs *NTFS) ProcessRecords(data []byte) {
+	var record MFT.Record
+	for i := 0; i < len(data); i += MFT.RecordSize {
+
+		record.Process(data[i : i+MFT.RecordSize])
+		ntfs.MFTRecords = append(ntfs.MFTRecords, record) //copies values
+	}
 }
 
 func (ntfs NTFS) GetMFTEntry(hD img.DiskReader, partitionOffset uint64,
@@ -30,7 +69,7 @@ func (ntfs NTFS) GetMFTEntry(hD img.DiskReader, partitionOffset uint64,
 	length := uint32(1024) // len of MFT record
 
 	buffer := make([]byte, length)
-	mftOffset := int64(ntfs.MFTOffset * uint64(ntfs.SectorsPerCluster) * 512)
+	mftOffset := int64(ntfs.MFTOffset * uint64(ntfs.SectorsPerCluster) * uint64(ntfs.BytesPerSector))
 	offsetedRecord := recordOffset
 	lengthB := 0
 	if offsetedRecord > 0 {
