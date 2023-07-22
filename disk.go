7269ddcd9aaf36c1462a35b3d37a3c905cf2dbf1
aarsakian <aarsakian@gmail.com>
2023-07-22 03:14:40 -0700

    consolidating mbr/gpt methods

---
+++ disk.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	gptLib "github.com/aarsakian/MFTExtractor/GPT"
+	mbrLib "github.com/aarsakian/MFTExtractor/MBR"
+	ntfsLib "github.com/aarsakian/MFTExtractor/NTFS"
+)
+
+type Disk struct {
+	physicalDriveNum int
+	partitionNum     int
+}
+
+type Partition interface {
+	GetOffset() uint64
+	LocateFileSystem(int) ntfsLib.NTFS
+}
+
+func (disk Disk) GetPhysicalPath() string {
+	return fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.physicalDriveNum)
+}
+
+func (disk Disk) GetPartition() Partition {
+
+	mbr := mbrLib.Parse(disk.physicalDriveNum)
+
+	if mbr.IsProtective() {
+
+		gpt := gptLib.Parse(disk.physicalDriveNum)
+		return gpt.GetPartition(disk.partitionNum)
+	} else {
+		return mbr.GetPartition(disk.partitionNum)
+	}
+}
