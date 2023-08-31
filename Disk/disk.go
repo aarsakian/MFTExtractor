f679e61bdedcac17525f2e1a5a5662027806122c
aarsakian <aarsakian@gmail.com>
2023-08-31 02:39:24 -0700

    read functionality moved/added working with images

--- Disk/disk.go
+++ Disk/disk.go
@@ -3,6 +3,7 @@ package disk
 import (
 	"fmt"
 
+	ewfLib "github.com/aarsakian/EWF_Reader/ewf"
 	gptLib "github.com/aarsakian/MFTExtractor/GPT"
 	mbrLib "github.com/aarsakian/MFTExtractor/MBR"
 	ntfsLib "github.com/aarsakian/MFTExtractor/NTFS"
@@ -12,6 +13,7 @@ import (
 type Disk struct {
 	PhysicalDriveNum int
 	PartitionNum     int
+	Image            *ewfLib.EWF_Image
 }
 
 type Partition interface {
@@ -28,12 +30,50 @@ func (disk Disk) GetHandler() img.DiskReader {
 }
 
 func (disk Disk) GetPartition() Partition {
+	var mbr mbrLib.MBR
+	physicalOffset := int64(0)
+	length := uint32(512) // MBR always at first sector
+	buffer := make([]byte, length)
+	if disk.Image != nil {
+		buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
 
-	mbr := mbrLib.Parse(disk.PhysicalDriveNum)
+	} else {
+		hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
+		hD.ReadFile(physicalOffset, buffer) // read 1st sector
+		defer hD.CloseHandler()
+	}
+
+	mbr.Parse(buffer)
 
 	if mbr.IsProtective() {
 
-		gpt := gptLib.Parse(disk.PhysicalDriveNum)
+		physicalOffset = int64(512) // gpt always starts at 512
+		length = uint32(512)
+		buffer = make([]byte, length)
+		if disk.Image != nil {
+			buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
+		} else {
+			hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
+			hD.ReadFile(physicalOffset, buffer)
+			defer hD.CloseHandler()
+
+		}
+
+		var gpt gptLib.GPT
+		gpt.ParseHeader(buffer)
+
+		buffer = make([]byte, gpt.GetPartitionArraySize())
+
+		if disk.Image != nil {
+			buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
+		} else {
+			hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
+			hD.ReadFile(int64(gpt.Header.PartitionsStartLBA*512), buffer)
+			defer hD.CloseHandler()
+		}
+
+		gpt.ParsePartitions(buffer)
+
 		return gpt.GetPartition(disk.PartitionNum)
 	} else {
 		return mbr.GetPartition(disk.PartitionNum)
