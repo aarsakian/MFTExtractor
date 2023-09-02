7820f0a0e2546bb837e3e79b9e05db1079edf1df
aarsakian <aarsakian@gmail.com>
2023-09-02 12:21:12 -0700

    introduced Partition/moved code

--- Disk/disk.go
+++ Disk/disk.go
@@ -4,21 +4,17 @@ import (
 	"fmt"
 
 	ewfLib "github.com/aarsakian/EWF_Reader/ewf"
-	gptLib "github.com/aarsakian/MFTExtractor/GPT"
-	mbrLib "github.com/aarsakian/MFTExtractor/MBR"
-	ntfsLib "github.com/aarsakian/MFTExtractor/NTFS"
+	"github.com/aarsakian/MFTExtractor/FS"
+	gptLib "github.com/aarsakian/MFTExtractor/Partition/GPT"
+	mbrLib "github.com/aarsakian/MFTExtractor/Partition/MBR"
 	"github.com/aarsakian/MFTExtractor/img"
 )
 
 type Disk struct {
 	PhysicalDriveNum int
-	PartitionNum     int
 	Image            *ewfLib.EWF_Image
-}
-
-type Partition interface {
-	GetOffset() uint64
-	LocateFileSystem(int) ntfsLib.NTFS
+	MBR              *mbrLib.MBR
+	GPT              *gptLib.GPT
 }
 
 func (disk Disk) GetPhysicalPath() string {
@@ -29,7 +25,18 @@ func (disk Disk) GetHandler() img.DiskReader {
 	return img.GetHandler(disk.GetPhysicalPath())
 }
 
-func (disk Disk) GetPartition() Partition {
+func (disk Disk) hasProtectiveMBR() bool {
+	return disk.MBR.IsProtective()
+}
+
+type Partitions []Partition
+
+type Partition interface {
+	GetOffset() uint64
+	LocateFileSystem([]byte) FS.FileSystem
+}
+
+func (disk *Disk) populateMBR() {
 	var mbr mbrLib.MBR
 	physicalOffset := int64(0)
 	length := uint32(512) // MBR always at first sector
@@ -38,44 +45,81 @@ func (disk Disk) GetPartition() Partition {
 		buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
 
 	} else {
-		hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
+		hD := img.GetHandler(disk.GetPhysicalPath())
 		hD.ReadFile(physicalOffset, buffer) // read 1st sector
 		defer hD.CloseHandler()
 	}
 
 	mbr.Parse(buffer)
 
-	if mbr.IsProtective() {
+	disk.MBR = &mbr
 
-		physicalOffset = int64(512) // gpt always starts at 512
-		length = uint32(512)
-		buffer = make([]byte, length)
-		if disk.Image != nil {
-			buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
-		} else {
-			hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
-			hD.ReadFile(physicalOffset, buffer)
-			defer hD.CloseHandler()
+}
 
-		}
+func (disk *Disk) populateGPT() {
 
-		var gpt gptLib.GPT
-		gpt.ParseHeader(buffer)
-
-		buffer = make([]byte, gpt.GetPartitionArraySize())
-
-		if disk.Image != nil {
-			buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
-		} else {
-			hD := img.GetHandler(fmt.Sprintf("\\\\.\\PHYSICALDRIVE%d", disk.PhysicalDriveNum))
-			hD.ReadFile(int64(gpt.Header.PartitionsStartLBA*512), buffer)
-			defer hD.CloseHandler()
-		}
-
-		gpt.ParsePartitions(buffer)
-
-		return gpt.GetPartition(disk.PartitionNum)
+	physicalOffset := int64(512) // gpt always starts at 512
+	length := uint32(512)
+	buffer := make([]byte, length)
+	if disk.Image != nil {
+		buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
 	} else {
-		return mbr.GetPartition(disk.PartitionNum)
+		hD := img.GetHandler(disk.GetPhysicalPath())
+		hD.ReadFile(physicalOffset, buffer)
+		defer hD.CloseHandler()
+
 	}
+
+	var gpt gptLib.GPT
+	gpt.ParseHeader(buffer)
+	length = gpt.GetPartitionArraySize()
+	buffer = make([]byte, length)
+
+	if disk.Image != nil {
+		buffer = disk.Image.RetrieveData(physicalOffset, int64(length))
+	} else {
+		hD := img.GetHandler(disk.GetPhysicalPath())
+		hD.ReadFile(int64(gpt.Header.PartitionsStartLBA*512), buffer)
+		defer hD.CloseHandler()
+	}
+
+	gpt.ParsePartitions(buffer)
+
+	disk.GPT = &gpt
+}
+
+func (disk *Disk) Populate() {
+	disk.populateMBR()
+	if disk.hasProtectiveMBR() {
+		disk.populateGPT()
+	}
+}
+
+func (disk Disk) GetSelectedPartition(partitionNum int) Partition {
+	var partition Partition
+	if disk.hasProtectiveMBR() {
+		partition = disk.GPT.GetPartition(partitionNum)
+	} else {
+		partition = disk.MBR.GetPartition(partitionNum)
+	}
+	return partition
+
+}
+
+func (disk Disk) ListPartitions() {
+
+	if disk.hasProtectiveMBR() {
+
+		partitions := disk.GPT.Partitions
+		for _, partition := range partitions {
+			fmt.Printf("%s\n", partition.GetPartitionType())
+		}
+	} else {
+		partitions := disk.MBR.Partitions
+		for _, partition := range partitions {
+			fmt.Printf("%s\n", partition.GetPartitionType())
+		}
+
+	}
+
 }
