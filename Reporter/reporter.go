751b0246bd8967ba303df35f88765fe6a0d38d1a
aarsakian <aarsakian@gmail.com>
2023-09-04 08:59:58 -0700

    flag introduced to handle print new line in reporting

--- Reporter/reporter.go
+++ Reporter/reporter.go
@@ -19,43 +19,54 @@ type Reporter struct {
 
 func (rp Reporter) Show(records []MFT.Record) {
 	for _, record := range records {
+		askedToShow := false
 		if record.Signature == "" { //empty record
 			continue
 		}
 		if rp.ShowFileName != "" {
 			record.ShowFileName(rp.ShowFileName)
+			askedToShow = true
 		}
 
 		if rp.ShowAttributes != "" {
 			record.ShowAttributes(rp.ShowAttributes)
+			askedToShow = true
 		}
 
 		if rp.ShowTimestamps {
 			record.ShowTimestamps()
+			askedToShow = true
 		}
 
 		if rp.IsResident {
 			record.ShowIsResident()
+			askedToShow = true
 		}
 
 		if rp.ShowRunList {
 			record.ShowRunList()
+			askedToShow = true
 		}
 
 		if rp.ShowFileSize {
 			record.ShowFileSize()
+			askedToShow = true
 		}
 
 		if rp.ShowVCNs {
 			record.ShowVCNs()
+			askedToShow = true
 		}
 
 		if rp.ShowIndex {
 
 			record.ShowIndex()
+			askedToShow = true
 		}
 
-		fmt.Printf("\n")
+		if askedToShow {
+			fmt.Printf("\n")
+		}
 
 	}
 
