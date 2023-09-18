a0f349f7a30cee4bb11a2d6c141fd91cccbbf515
aarsakian <aarsakian@gmail.com>
2023-09-18 12:33:20 -0700

    added name in attrlist

--- FS/NTFS/MFT/attributes/attrlist.go
+++ FS/NTFS/MFT/attributes/attrlist.go
@@ -41,7 +41,7 @@ func (attrListEntries *AttributeListEntries) Parse(data []byte) {
 		var attrList AttributeList
 		utils.Unmarshal(data[attrLen:attrLen+24], &attrList)
 		attrList.Name = utils.NoNull(data[attrLen+uint16(attrList.Nameoffset) : attrLen+uint16(attrList.Nameoffset)+2*uint16(attrList.Namelen)])
-		//   runlist=bs[ReadPtr+atrRecordResident.OffsetContent+attrList.len:uint32(ReadPtr)+atrRecordResident.Len]
+
 		attrListEntries.Entries = append(attrListEntries.Entries, attrList)
 		attrLen += attrList.Len
 
