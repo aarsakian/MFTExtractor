ee052f2af582850d5dd28e633b6a804d91292091
aarsakian <aarsakian@gmail.com>
2023-09-22 12:18:00 -0700

    moved vol attributes to their file

---
+++ FS/NTFS/MFT/attributes/volattributes.go
@@ -0,0 +1,67 @@
+package attributes
+
+import "github.com/aarsakian/MFTExtractor/utils"
+
+type VolumeName struct {
+	Name   utils.NoNull
+	Header *AttributeHeader
+}
+
+type VolumeInfo struct {
+	F1     uint64 //unused
+	MajVer string // 8-8
+	MinVer string // 9-9
+	Flags  uint16 //see table 13.22
+	F2     uint32
+	Header *AttributeHeader
+}
+
+
+func (volInfo *VolumeInfo) SetHeader(header *AttributeHeader) {
+	volInfo.Header = header
+}
+
+func (volInfo VolumeInfo) GetHeader() AttributeHeader {
+	return *volInfo.Header
+}
+
+func (volInfo *VolumeInfo) Parse(data []byte) {
+	utils.Unmarshal(data, volInfo)
+}
+
+func (volInfo VolumeInfo) IsNoNResident() bool {
+	return volInfo.Header.IsNoNResident()
+}
+
+func (volInfo VolumeInfo) FindType() string {
+	return volInfo.Header.GetType()
+}
+
+func (volinfo VolumeInfo) ShowInfo() {
+
+}
+
+func (volName *VolumeName) SetHeader(header *AttributeHeader) {
+	volName.Header = header
+}
+
+func (volName VolumeName) GetHeader() AttributeHeader {
+	return *volName.Header
+}
+
+func (volName *VolumeName) Parse(data []byte) {
+	volName.Name = utils.NoNull(data)
+
+}
+
+func (volName VolumeName) FindType() string {
+	return volName.Header.GetType()
+}
+
+func (volName VolumeName) IsNoNResident() bool {
+	return volName.Header.IsNoNResident()
+}
+
+func (volName VolumeName) ShowInfo() {
+
+}
