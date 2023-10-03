a1f798de22f57fc38d08af0c7d3e1a95ed6f6d8d
aarsakian <aarsakian@gmail.com>
2023-10-03 13:25:37 -0700

    FS interface changes

--- FS/filesystem.go
+++ FS/filesystem.go
@@ -1,14 +1,15 @@
 package FS
 
 import (
+	ntfs "github.com/aarsakian/MFTExtractor/FS/NTFS"
 	"github.com/aarsakian/MFTExtractor/FS/NTFS/MFT"
 	"github.com/aarsakian/MFTExtractor/img"
 )
 
 type FileSystem interface {
-	Process(img.DiskReader, int64, int, int, int)
+	Process(img.DiskReader, int64, []int, int, int)
 	GetSectorsPerCluster() int
 	GetBytesPerSector() uint64
-	GetFileContents(img.DiskReader, int64) map[string][]byte
+	GetFileContents(img.DiskReader, int64, chan ntfs.Task)
 	GetMetadata() []MFT.Record
 }
