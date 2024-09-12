790827c33b49f7991fa1a1f7a26d96628a7339da
Armen Arsakian <aarsakian@gmail.com>
2024-09-12 09:45:37 +0300

    support form EA,EA_Information,LoggedUtility

---
+++ FS/NTFS/MFT/attributes/eaattr.go
@@ -0,0 +1,72 @@
+package attributes
+
+import "github.com/aarsakian/MFTExtractor/utils"
+
+type EA_INFORMATION struct {
+	SizeOfEntry uint16
+	NofEA       uint16
+	Size        uint32
+	Header      *AttributeHeader
+}
+
+type ExtendedAttribute struct {
+	NextAttrOffset int32
+	Flags          uint8
+	NofChars       uint8
+	DataSize       uint16
+	Name           []byte
+	Value          []byte
+	Header         *AttributeHeader
+}
+
+func (ea_info EA_INFORMATION) FindType() string {
+	return ea_info.Header.GetType()
+}
+
+func (ea_info *EA_INFORMATION) SetHeader(header *AttributeHeader) {
+	ea_info.Header = header
+}
+
+func (ea_info EA_INFORMATION) GetHeader() AttributeHeader {
+	return *ea_info.Header
+}
+
+func (ea_info EA_INFORMATION) IsNoNResident() bool {
+	return ea_info.Header.IsNoNResident()
+}
+
+func (ea_info *EA_INFORMATION) Parse(data []byte) {
+
+	utils.Unmarshal(data, ea_info)
+
+}
+
+func (ea_info *EA_INFORMATION) ShowInfo() {
+
+}
+
+func (ea ExtendedAttribute) FindType() string {
+	return ea.Header.GetType()
+}
+
+func (ea *ExtendedAttribute) SetHeader(header *AttributeHeader) {
+	ea.Header = header
+}
+
+func (ea ExtendedAttribute) GetHeader() AttributeHeader {
+	return *ea.Header
+}
+
+func (ea ExtendedAttribute) IsNoNResident() bool {
+	return ea.Header.IsNoNResident()
+}
+
+func (ea *ExtendedAttribute) Parse(data []byte) {
+
+	utils.Unmarshal(data, ea)
+
+}
+
+func (ea ExtendedAttribute) ShowInfo() {
+
+}
---
+++ FS/NTFS/MFT/attributes/loggedutility.go
@@ -0,0 +1,50 @@
+package attributes
+
+import "github.com/aarsakian/MFTExtractor/utils"
+
+type LoggedUtilityStream struct {
+	Kind    string
+	TXFDATA *TXFDATA
+	Header  *AttributeHeader
+}
+
+type TXFDATA struct {
+	ParRef              uint64
+	ParSeq              uint16
+	Flags               [8]byte //
+	USN                 uint64  //
+	TxId                uint64  //
+	LSN_NTFS_Metadata   uint64  //
+	LSN_User_Data       uint64  //
+	LSN_Directory_Index uint64  //
+
+}
+
+func (loggedUtility LoggedUtilityStream) FindType() string {
+	return loggedUtility.Header.GetType()
+}
+
+func (loggedUtility *LoggedUtilityStream) SetHeader(header *AttributeHeader) {
+	loggedUtility.Header = header
+}
+
+func (loggedUtility LoggedUtilityStream) GetHeader() AttributeHeader {
+	return *loggedUtility.Header
+}
+
+func (loggedUtility LoggedUtilityStream) IsNoNResident() bool {
+	return loggedUtility.Header.IsNoNResident()
+}
+
+func (loggedUtility *LoggedUtilityStream) Parse(data []byte) {
+	if loggedUtility.Kind == "$TXF_DATA" {
+		txfData := new(TXFDATA)
+		utils.Unmarshal(data, txfData)
+		loggedUtility.TXFDATA = txfData
+	}
+
+}
+
+func (loggedUtiltiy LoggedUtilityStream) ShowInfo() {
+
+}
