94d501d47c50b770d4c7d469d27189597e06b7c9
Armen Arsakian <aarsakian@gmail.com>
2024-12-01 20:45:03 +0200

    rewrote tree creation using entry ranges to speed up building time

--- tree/builder.go
+++ tree/builder.go
@@ -14,9 +14,11 @@ in the tree is actually the number of the block containing the child node (usual
 interpreted as an offset from the beginning of the corresponding disk file)*/
 
 type Node struct {
-	record   *MFT.Record
-	parent   *Node
-	children []*Node
+	record        *MFT.Record
+	parent        *Node
+	children      []*Node
+	MinChildEntry uint32
+	MaxChildEntry uint32
 }
 
 type Tree struct {
@@ -24,11 +26,15 @@ type Tree struct {
 }
 
 func (t *Tree) Build(records MFT.Records) {
-	fmt.Printf("Building tree from MFT records \n")
+	msg := fmt.Sprintf("Building tree from %d MFT records ", len(records))
+	fmt.Printf(msg + "\n")
+	logger.MFTExtractorlogger.Info(msg)
+
 	for idx := range records {
 		if records[idx].Entry < 5 { //$MFT entry number 5
 			continue
 		}
+
 		t.AddRecord(&records[idx])
 	}
 
@@ -39,7 +45,7 @@ func (t *Tree) AddRecord(record *MFT.Record) {
 	if t.root == nil {
 		logger.MFTExtractorlogger.Info("initialized root")
 
-		t.root = &Node{record, nil, nil}
+		t.root = &Node{record, nil, nil, 0, 0}
 	} else {
 
 		t.root.insert(record)
@@ -47,21 +53,58 @@ func (t *Tree) AddRecord(record *MFT.Record) {
 
 }
 
-func (n *Node) insert(record *MFT.Record) {
-	if record.FindAttribute("FileName") != nil {
-		fnattr := record.FindAttribute("FileName").(*MFTAttributes.FNAttribute)
-		if uint64(n.record.Entry) == fnattr.ParRef && n.record.Seq-fnattr.ParSeq < 2 { //record is children
-			childNode := Node{record, n, nil}
-			n.children = append(n.children, &childNode)
+func (node *Node) insert(record *MFT.Record) {
+	if !node.record.IsFolder() {
+		return
+	}
 
-			logger.MFTExtractorlogger.Info(fmt.Sprintf("added child %s Id %d  to %s Id %d", record.GetFname(), record.Entry, n.record.GetFname(), n.record.Entry))
+	attr := record.FindAttribute("FileName")
+	if attr != nil {
+		fnattr := attr.(*MFTAttributes.FNAttribute)
+		if uint64(node.record.Entry) == fnattr.ParRef && node.record.Seq-fnattr.ParSeq < 2 { //record is children
+			childNode := Node{record, node, nil, 0, 0}
+			node.children = append(node.children, &childNode)
+			node.updateEntryRange(record.Entry)
+
+			childNode.parent = &childNode
+
+			logger.MFTExtractorlogger.Info(fmt.Sprintf("added child %s Id %d  to %s Id %d", record.GetFname(), record.Entry, node.record.GetFname(), node.record.Entry))
 
 		} else {
-			for idx := range n.children { //test its children
-				n.children[idx].insert(record)
+			logger.MFTExtractorlogger.Info(fmt.Sprintf("checking children of %s with Id %d for %s %d ",
+				node.record.GetFname(), node.record.Entry, record.GetFname(), fnattr.ParRef))
+			for idx := range node.children { //test its children
+				if !node.contains(uint32(fnattr.ParRef)) {
+					continue
+				}
+				node.children[idx].insert(record)
 
 			}
 		}
+	}
+
+}
+
+func (node *Node) contains(entry uint32) bool {
+
+	if node.MinChildEntry <= entry && entry <= node.MaxChildEntry {
+		return true
+
+	}
+
+	return false
+}
+
+func (node *Node) updateEntryRange(entry uint32) {
+	for node != nil {
+		if node.MinChildEntry > entry {
+			node.MinChildEntry = entry
+		}
+		if node.MaxChildEntry < entry {
+			node.MaxChildEntry = entry
+		}
+		node = node.parent
+
 	}
 
 }
@@ -72,21 +115,21 @@ func (t Tree) Show() {
 
 }
 
-func (n Node) Show() {
-	if n.children == nil {
+func (node Node) Show() {
+	if node.children == nil {
 		return
 
 	}
 	msgB := strings.Builder{}
-	msgB.Grow(len(n.children) + 1) // for root
+	msgB.Grow(len(node.children) + 1) // for root
 
-	msg := fmt.Sprintf(" %s  |_> ", n.record.GetFname())
+	msg := fmt.Sprintf(" %s  |_> ", node.record.GetFname())
 
 	msgB.WriteString(msg)
 
 	fmt.Print("\n" + msg)
 
-	for _, childnode := range n.children {
+	for _, childnode := range node.children {
 		msg := fmt.Sprintf(" %s", childnode.record.GetFname())
 
 		fmt.Print(msg)
@@ -96,7 +139,7 @@ func (n Node) Show() {
 
 	logger.MFTExtractorlogger.Info(msgB.String())
 
-	for _, node := range n.children {
+	for _, node := range node.children {
 
 		node.Show()
 
